e75a193113f9a42c9db396c1be1cbd36eb7391b4
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-24 00:10:28 +0100

    Update betabot.py

--- betabot.py
+++ betabot.py
@@ -6,7 +6,7 @@ import time
 import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = ";")
+client = commands.Bot(command_prefix = "=")
 @client.event
 async def on_ready():
     print("Thankyou For Using Jackaboi Beta Bot!")
@@ -14,10 +14,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith(';hello'):
+    if message.content.startswith('=hello'):
         msg = 'Hello {0.author.mention} How Are You Today'.format(message)
         await client.send_message(message.channel, msg)
-    if message.content.startswith(';bye'):
+    if message.content.startswith('=bye'):
         msg = 'Goodbye {0.author.mention} Hope To See You Soon :wave:'.format(message)
         await client.send_message(message.channel, msg)      
 
