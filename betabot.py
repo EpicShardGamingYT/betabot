5285cc0da2cbd6a76c74743f48859d8ba2d1f7a5
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-24 15:24:29 +0100

    Update betabot.py

--- betabot.py
+++ betabot.py
@@ -23,4 +23,18 @@ async def on_message(message):
     if message.content.startswith('=test'):
         msg = 'Goodbye \n{0.author.mention}\n Hope To See You Soon'.format(message)
         await client.send_message(message.channel, msg)    
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 client.run(os.getenv('TOKEN'))
