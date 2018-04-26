6168fa284f04b5c86b4b6410e22e375302b4588a
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-26 23:56:27 +0100

    Update betabot.py

--- betabot.py
+++ betabot.py
@@ -18,11 +18,10 @@ async def on_message(message):
         msg = 'Hello {0.author.mention} How Are You Today'.format(message)
         await client.send_message(message.channel, msg)
     if message.author.id == "344967220025098242":
-    if message.content.lower().startswith('=test'):
-        msg = 'Hello {message.author.mention} How Are You Today'
+     if message.content.startswith('=test'):
+        msg = 'Hello {0.author.mention} How Are You Today'.format(message)
         await client.send_message(message.channel, msg)
-else:
-    testddd
-    
+else: 
     only Jackaboi#8319 Can Use This Command
+    
 client.run(os.getenv('TOKEN'))
