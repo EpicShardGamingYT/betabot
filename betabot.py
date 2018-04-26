12017a9f6a54671c10da0a7e4efa30c4cae7ea40
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-26 23:58:15 +0100

    Update betabot.py

--- betabot.py
+++ betabot.py
@@ -18,7 +18,7 @@ async def on_message(message):
         msg = 'Hello {0.author.mention} How Are You Today'.format(message)
         await client.send_message(message.channel, msg)
     if message.author.id == "344967220025098242":
-    if message.content.startswith('=test'):
+     if message.content.startswith('=test'):
         msg = 'Hello {0.author.mention} How Are You Today'.format(message)
         await client.send_message(message.channel, msg)
  else: 
