e12c8c204887690e6a542ccf295e25a75e640249
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-04-26 21:33:15 +0100

    Update betabot.py

--- betabot.py
+++ betabot.py
@@ -18,24 +18,11 @@ async def on_message(message):
     if message.content.startswith('=hello'):
         msg = 'Hello {0.author.mention} How Are You Today'.format(message)
         await client.send_message(message.channel, msg)
-    if message.content.startswith('=bye'):
-        msg = 'Goodbye {0.author.mention} Hope To See You Soon :wave:'.format(message)
+    
+    if message.author.id == "344967220025098242":
+    if message.content.lower().startswith('=test'):
+        msg = 'Hello {message.author.mention} How Are You Today'
         await client.send_message(message.channel, msg)
-    if message.content.startswith('=test'):
-        msg = 'Goodbye \n{0.author.mention}\n Hope To See You Soon'.format(message)
-        await client.send_message(message.channel, msg)    
-@client.command(name='8ball',
-                description="Answers a yes/no question.",
-                brief="Answers from the beyond.",
-                aliases=['eight_ball', 'eightball', '8-ball'],
-                pass_context=True)
-async def eight_ball(context):
-    possible_responses = [
-        'That is a resounding no',
-        'It is not looking likely',
-        'Too hard to tell',
-        'It is quite possible',
-        'Definitely',
-    ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+else:
+    only Jackaboi#8319 Can Use This Command
 client.run(os.getenv('TOKEN'))
